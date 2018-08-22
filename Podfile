00e674e1926d7279868b7ee667d252055bfabadf
chi <chiyumyun@gmail.com>
2018-08-23 07:11:12 +0900

    コミットするの忘れていた。

---
+++ Podfile
@@ -0,0 +1,20 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'TapTap-iOS' do
+  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for TapTap-iOS
+pod 'SwiftyUserDefaults', '4.0.0-alpha.1'
+  target 'TapTap-iOSTests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+  target 'TapTap-iOSUITests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+end
--- TapTap-iOS/views/Game/GameViewController.swift
+++ TapTap-iOS/views/Game/GameViewController.swift
@@ -9,34 +9,61 @@
 import UIKit
 
 class GameViewController: UIViewController {
+    
+    var leftNum: Int = 0
+    var rightNum: Int = 0
+    var showCount: Double?
 
     @IBOutlet weak var scoreLabel: UILabel!
     @IBOutlet weak var rLabel: UILabel!
     @IBOutlet weak var lLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        leftNum = Int(arc4random_uniform(50))
+        rightNum = Int(arc4random_uniform(50))
+        showCount = Double(arc4random_uniform(UInt32(5.00)))
+//        print(showCount)
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
     }
     
+    func startTimer() {
+        
+    }
+    
+    /*
+     スタートボタン押下で、ラベルがラップされるまでのカウント開始
+     
+     マイランキングとタイトルボタンを消す
+     
+     カウントがラベルに表示されたらカウント開始
+     
+     ラベルがタップされたらタイムを表示
+     */
+    
+    
     @IBAction func oStartButton(_ sender: Any) {
-        self.dismiss(animated: true, completion: nil)
     }
     
     @IBAction func oRunkingButton(_ sender: Any) {
         let storyboard: UIStoryboard = UIStoryboard(name: "Ranking", bundle: nil)
         let nextView = storyboard.instantiateViewController(withIdentifier: "RankingViewController")
         self.present(nextView, animated: true, completion: nil)
-//        self.dismiss(animated: true, completion: nil)
     }
     
     @IBAction func oTitleButton(_ sender: Any) {
-        let storyboard: UIStoryboard = UIStoryboard(name: "Title", bundle: nil)
-        let nextView = storyboard.instantiateViewController(withIdentifier: "TitleViewController")
-        self.present(nextView, animated: true, completion: nil)
-//        self.dismiss(animated: true, completion: nil)
+        self.dismiss(animated: true, completion: nil)
+//        let storyboard: UIStoryboard = UIStoryboard(name: "Title", bundle: nil)
+//        let nextView = storyboard.instantiateViewController(withIdentifier: "TitleViewController")
+//        self.present(nextView, animated: true, completion: nil)
     }
     
 }
