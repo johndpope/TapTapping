e0db75a2002686633cba5f7d5d9fb861aa1ab083
chi <chiyumyun@gmail.com>
2018-09-27 06:36:12 +0900

    save commit

--- TapTap-iOS/views/Ranking/RankingViewController.swift
+++ TapTap-iOS/views/Ranking/RankingViewController.swift
@@ -15,21 +15,37 @@ class RankingViewController: UIViewController, UITableViewDelegate, UITableViewD
     
     let rankNum: [String] = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10"]
     
+    //MARK:- override
+    //    --------------------------------------------------------------------------------
+    //    viewDidLoad()
+    //    --------------------------------------------------------------------------------
     override func viewDidLoad() {
         super.viewDidLoad()
+//        self.navigationController?.popViewController(animated: true)
+        let leftBarButton = UIBarButtonItem(title: "<", style: .plain, target: self, action: #selector(RankingViewController.tappedLeftBarButton))
+        self.navigationItem.leftBarButtonItem = leftBarButton
     }
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
-//        readDate()
+        self.tableView.reloadData()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
     }
+    
+    @objc func tappedLeftBarButton() {
+        self.dismiss(animated: true, completion: nil)
+//        let previousPage = PreviousViewController()
+//        self.navigationController?.pushViewController(previousPage, animated: true)
+    }
 
+    //MARK:- delegate
+    //    --------------------------------------------------------------------------------
+    //    tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int
+    //    --------------------------------------------------------------------------------
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-//        return rankNum.count
         return Defaults[.scoreRecord].count
     }
     
@@ -37,14 +53,8 @@ class RankingViewController: UIViewController, UITableViewDelegate, UITableViewD
         let cell = tableView.dequeueReusableCell(withIdentifier: "myCell") as! RankingTableViewCell
         cell.backgroundColor = UIColor.clear
         cell.rankLabel?.text = rankNum[indexPath.row]
-//        let test = Defaults[.scoreRecord]
-//        let test = Defaults[.scoreRecord](repeating: 0, count: 9)
-//        print(test)
-//        let array = Defaults.scoreRecord(forKey: "scoreRecord") ?? [String]()
         let score = Defaults[.scoreRecord]
         cell.scoreLabel?.text = score[indexPath.row]
-        //        print(UserDefaults.standard.dictionaryRepresentation().values)
-
         return cell
     }
 }
